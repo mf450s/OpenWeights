e7e930e7bcef245fb79d0b8b0f73b119a9a7d859
Tom Becker <tomdastulpenlakritz@gmail.com>
2026-02-09 14:22:46 +0100

    chore: Remove Quick Start Guide and clean up Docker Compose example

--- src/Weights.Infrastructure/Data/Configurations/ExerciseConfiguration.cs
+++ src/Weights.Infrastructure/Data/Configurations/ExerciseConfiguration.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Weights.Domain.Entities;
-using Weights.Domain.Enums;
 
 namespace Weights.Infrastructure.Data.Configurations;
 
